e429458fea1ffbe6dbd0eaabf2ed75ebef930ba0
Salz777 <lesnik.ilay@gmail.com>
2025-10-14 10:53:05 +0500

    Add files via upload

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function Page() {
+    return (
+    <><h1 className={`${lusitana.className} text-2xl font-bold text-black-700`}> Страница панели управления </h1>
+    <p className={`${lusitana.className} text-lg font-normal text-black-400`}>Обычный шрифт</p></>
+    );
+}
